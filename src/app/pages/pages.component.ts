b0af3d9b15f34d0cef86f5c1781bfe2a4a5099f1
D24-Developer <rudy@designer-24.com>
2020-08-12 19:59:10 +0300

    mini

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -28,11 +28,10 @@ export class PagesComponent implements OnInit, AfterViewInit {
   public currencies = ['USD'];
   public currency:any;
   public slides = [
-    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/banner1.jpg' },
-    { title: 'Summer collection', subtitle: 'New Arrivals On Sale', image: 'assets/images/carousel/banner2.jpg' },
-    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/banner3.jpg' },
-    { title: 'Summer collection', subtitle: 'New Arrivals On Sale', image: 'assets/images/carousel/banner4.jpg' },
-    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/banner5.jpg' }
+    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/image1.jpg' },
+    { title: 'Summer collection', subtitle: 'New Arrivals On Sale', image: 'assets/images/carousel/image2.jpg' },
+    { title: 'The biggest sale', subtitle: 'Special for today', image: 'assets/images/carousel/image3.jpg' },
+    { title: 'Summer collection', subtitle: 'New Arrivals On Sale', image: 'assets/images/carousel/image4.jpg' }
   ];
   public flags = [
     { name:'English', image: 'assets/images/flags/gb.svg' },
